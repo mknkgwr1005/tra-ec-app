5820e1cf63dbfb1586578ba5ee48830b0d2f54d3
Mika Nakagawara <mika.nakagawara@rakus-partners.co.jp>
2022-08-09 09:57:35 +0900

    signup機能作成

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,8 +1,8 @@
-import firebase from "firebase/app";
-import "firebase/auth";
-import " firebase/firestore";
-import "firestore/storage";
-import "firebase/functions";
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import "firebase/compat/storage";
+import "firebase/compat/functions";
 import { firebaseConfig } from "./config";
 
 firebase.initializeApp(firebaseConfig);
@@ -11,4 +11,4 @@ export const auth = firebase.auth();
 export const db = firebase.firestore();
 export const storage = firebase.storage();
 export const functions = firebase.functions();
-export const FirebaseTimestamp = firebase.firestore.FirebaseTimestamp;
+export const FirebaseTimestamp = firebase.firestore.Timestamp;
--- src/reducks/reducks/users/operations.js
+++ src/reducks/reducks/users/operations.js
@@ -1,6 +1,7 @@
 // 非同期処理の制御。actionsを呼び出す
 import { push } from "connected-react-router";
 import { signInAction } from "./actions";
+import { auth, db, FirebaseTimestamp } from "../../../firebase/index";
 
 export const signIn = (email, password) => {
   // 第一引数：actionを呼び出す、getState:storeの参照
@@ -27,5 +28,51 @@ export const signIn = (email, password) => {
 
       dispatch(push("/"));
     }
+  };
+};
+
+export const signUp = (username, email, password, confirmPassword) => {
+  return async (dispatch) => {
+    if (
+      username == "" ||
+      password == "" ||
+      email == "" ||
+      confirmPassword == ""
+    ) {
+      alert("必須項目が未入力です");
+      return false;
+    }
+
+    if (password !== confirmPassword) {
+      alert("パスワードが一致していません。もう一度入力してください");
+      return false;
+    }
+
+    return auth
+      .createUserWithEmailAndPassword(email, password)
+      .then((result) => {
+        console.log(result);
+        const user = result.user;
+        if (user) {
+          const uid = user.uid;
+          const timeStamp = FirebaseTimestamp.now();
+
+          const userInitialData = {
+            created_at: timeStamp,
+            email: email,
+            role: "customer",
+            uid: uid,
+            updated_at: timeStamp,
+            username: username,
+          };
+
+          db.collection("users")
+            .doc(uid)
+            .set(userInitialData)
+            .then(() => {
+              dispatch(push("/"));
+            });
+        }
+      });
   };
 };
