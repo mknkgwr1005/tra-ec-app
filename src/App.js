c2679bf3c6536d3644ccf6a8840e6b2aa2fab7bb
mknkgwr1005 <mikan941005@gmail.com>
2022-12-19 13:19:30 +0900

    不足しているファイルを追加

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
-import React, { Component } from "react";
+import React from "react";
+import Router from "./Router";
+import "./assets/reset.css";
+import "./assets/style.css";
+import { Header } from "./components/Uikit/Header";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <main className="c-main">
+        <Router />
+      </main>
+    </>
   );
-}
+};
 
 export default App;
